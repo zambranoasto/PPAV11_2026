849eceaedb8b500a12b5288cee5269663ca50015
zambranoasto <zambrano.asto@gmail.com>
2026-04-09 00:16:09 -0700

    Update enrichment_analysis.R

--- enrichment_analysis.R
+++ enrichment_analysis.R
@@ -1,16 +1,16 @@
-# Functional enrichment and pathway analysis using GO, KEGG, and REACTOME
+# Functional enrichment and pathway analysis of PPAV11 using GO, KEGG, and REACTOME databases
 
-# Packages
+# Import required libraries
 library(clusterProfiler)
 library(org.Hs.eg.db)
 library(ReactomePA)
 library(circlize)
 library(RColorBrewer)
 
-# Protein list
+# Define protein list
 genes_symbol <- c("CHI3L1", "CYCS", "DDAH1", "GDA", "LRRC4B", "NPTX2", "PKM", "SMOC1", "SPON1", "YWHAG", "YWHAZ")
 
-# GO enrichment (BP, CC, MF)
+# Run GO enrichment analyses (BP, CC, MF)
 ontologies <- c("BP", "CC", "MF")
 go_results <- lapply(ontologies, function(ont) {
   enrichGO(gene = genes_symbol, OrgDb = org.Hs.eg.db,
@@ -32,12 +32,12 @@ prep_terms <- function(res, category) {
 }
 terms_go <- do.call(rbind, Map(prep_terms, go_results, ontologies))
 
-# Conversion to Entrez ID
+# Convert Gene symbol to Entrez ID
 genes_entrez_df <- bitr(genes_symbol, fromType = "SYMBOL",
                         toType = "ENTREZID", OrgDb = org.Hs.eg.db)
 genes_entrez <- genes_entrez_df$ENTREZID
 
-# KEGG and Reactome enrichment
+# Run KEGG and REACTOME enrichment analysis
 ekegg <- enrichKEGG(gene = genes_entrez, organism = "hsa", pvalueCutoff = 0.05)
 ekegg <- setReadable(ekegg, OrgDb = org.Hs.eg.db, keyType = "ENTREZID")
 ereact <- enrichPathway(gene = genes_entrez, organism = "human",
@@ -58,22 +58,17 @@ terms_kegg   <- prep_pathway(ekegg, "KEGG")
 terms_react  <- prep_pathway(ereact, "RP")
 
 # Save results
-write.csv(terms_go, "GOenrichmentPPAV11.csv", row.names = FALSE)
-write.csv(rbind(terms_kegg, terms_react), "Pathway_enrichment_filtered.csv", row.names = FALSE)
+write.csv(terms_go, "Output_GO.csv", row.names = FALSE)
+write.csv(rbind(terms_kegg, terms_react), "Output_kegg_reactome.csv", row.names = FALSE)
 
-# Fixed color palette per protein
+# Assign colors to proteins
 colors_genes <- setNames(c("olivedrab3", "darkorchid4", "blue4",
                             "aquamarine3", "darkkhaki", "cyan3",
                             "plum3", "violet", "darkseagreen2",
-                            "deeppink2", "gold1", "cornflowerblue", "chocolate1",
-                            "coral2", "maroon4", "darkmagenta", "darkcyan",
-                            "darkred", "chartreuse2", "cadetblue2",
-                            "azure2", "yellow1", "hotpink2", "brown4", "mediumpurple3",
-                            "bisque2", "dodgerblue2", "forestgreen", "yellowgreen",
-                            "skyblue", "violetred", "pink4", "seagreen3")[1:length(genes_symbol)],
+                            "deeppink2")[1:length(genes_symbol)],
                           genes_symbol) 
 
-# GO plot
+# Generate GO plot
 long_go <- do.call(rbind, lapply(1:nrow(terms_go), function(i) {
   genes <- unlist(strsplit(terms_go$genes[i], "/"))
   genes <- genes[genes %in% names(colors_genes)]
@@ -151,7 +146,7 @@ generate_GO_diagram <- function(genes_symbol, file_name = "GOdiagram.pdf") {
 return(terms_go)
 }
 
-# KEGG and Reactome plot
+# Generate KEGG and Reactome plot
 terms_pathways <- rbind(terms_kegg, terms_react)
 long_path <- do.call(rbind, lapply(1:nrow(terms_pathways), function(i) {
   genes <- unlist(strsplit(terms_pathways$genes[i], "/"))
